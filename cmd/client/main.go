35e583d5659947d0a844d3eb78f8d3113f6cca1d
Eugene Owak <teugeneowak@gmail.com>
2026-08-26 18:30:43 +0300

    Declare and bind a queue on client startup

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/geneowak/go-pub-sub/internal/gamelogic"
+	"github.com/geneowak/go-pub-sub/internal/pubsub"
+	"github.com/geneowak/go-pub-sub/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	const connString = "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(connString)
+	if err != nil {
+		log.Fatal("Unable to connect to rabbitmq server: ", err)
+	}
+	defer conn.Close()
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, _, err = pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, fmt.Sprintf("%s.%s", routing.PauseKey, username), routing.PauseKey, pubsub.Transient)
+	if err != nil {
+		log.Fatal("Failed to declare and bind", err)
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	<-ctx.Done()
+
+	fmt.Println("Shutting down...")
+	time.Sleep(time.Second)
+	fmt.Println("Program exited.")
+
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -14,13 +14,44 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 		return fmt.Errorf("Failed to marshal val: %w", err)
 	}
 
-	err = ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{
+	return ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{
 		ContentType: "application/json",
 		Body:        result,
 	})
+}
+
+type SimpleQueueType string
+
+const (
+	Transient SimpleQueueType = "transient"
+	Durable   SimpleQueueType = "durable"
+)
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType,
+) (*amqp.Channel, amqp.Queue, error) {
+
+	channel, err := conn.Channel()
 	if err != nil {
-		return fmt.Errorf("Failed to publish: %w", err)
+		return nil, amqp.Queue{}, err
 	}
 
-	return nil
+	queue, err := channel.QueueDeclare(
+		queueName,
+		queueType == Durable,
+		queueType == Transient,
+		queueType == Transient,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+	channel.QueueBind(queueName, key, exchange, false, nil)
+
+	return channel, queue, nil
 }
