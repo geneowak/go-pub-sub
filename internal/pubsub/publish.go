8c49692191952efddcce2f3059436c8a1a6f90fa
Eugene Owak <teugeneowak@gmail.com>
2026-09-02 18:35:06 +0300

    isolate publish logic into separate file

---
+++ internal/pubsub/publish.go
@@ -0,0 +1,59 @@
+package pubsub
+
+import (
+	"bytes"
+	"context"
+	"encoding/gob"
+	"encoding/json"
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishJSON[T any](
+	ch *amqp.Channel,
+	exchange, key string,
+	val T,
+) error {
+	result, err := json.Marshal(val)
+	if err != nil {
+		return fmt.Errorf("Failed to marshal val: %w", err)
+	}
+
+	return ch.PublishWithContext(
+		context.Background(),
+		exchange,
+		key,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        result,
+		},
+	)
+}
+
+func PublishGob[T any](
+	ch *amqp.Channel,
+	exchange, key string,
+	val T,
+) error {
+	var buffer bytes.Buffer
+	encoder := gob.NewEncoder(&buffer)
+	err := encoder.Encode(val)
+	if err != nil {
+		return fmt.Errorf("Failed to encode val: %w", err)
+	}
+
+	return ch.PublishWithContext(
+		context.Background(),
+		exchange,
+		key,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/gob",
+			Body:        buffer.Bytes(),
+		},
+	)
+}
